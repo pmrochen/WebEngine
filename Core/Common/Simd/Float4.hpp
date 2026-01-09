a2ce54164c2d64da14af319754f0355050697a12
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-01-09 17:12:44 +0100

    Add swizzle functions

--- Core/Common/Simd/Float4.hpp
+++ Core/Common/Simd/Float4.hpp
@@ -18,97 +18,97 @@ struct Float4
 	struct Bool4
 	{
 		Bool4() = default;
-        explicit Bool4(const bool s) noexcept { value = float4::set4(s); }
-        Bool4(const bool x, const bool y, const bool z, const bool w) noexcept { value = float4::set4(x, y, z, w); }
-		Bool4(const float4::Type b) noexcept : value(b) {}
+        explicit Bool4(bool s) noexcept { value = set4(s); }
+        Bool4(bool x, bool y, bool z, bool w) noexcept { value = set4(x, y, z, w); }
+		Bool4(float4 b) noexcept : value(b) {}
 
-		operator float4::Type() const noexcept { return value; }
-		Bool4 operator!() const noexcept { return Bool4(float4::not4(value)); }
-        //friend Bool4 operator&&(const Bool4 b1, const Bool4 b2) noexcept { return Bool4(float4::and4(b1, b2)); }
-        //friend Bool4 operator||(const Bool4 b1, const Bool4 b2) noexcept { return Bool4(float4::or4(b1, b2)); }
+		operator float4() const noexcept { return value; }
+		Bool4 operator!() const noexcept { return Bool4(not4(value)); }
+        //friend Bool4 operator&&(const Bool4 b1, const Bool4 b2) noexcept { return Bool4(and4(b1, b2)); }
+        //friend Bool4 operator||(const Bool4 b1, const Bool4 b2) noexcept { return Bool4(or4(b1, b2)); }
 
-		float4::Type value;
+		float4 value;
 	};
 
 	Float4() = default;
-	explicit Float4(const float s) noexcept { value = float4::set4(s); }
-	Float4(const float x, const float y, const float z, const float w) noexcept { value = float4::set4(x, y, z, w); }
-	Float4(const float4::Type v) noexcept : value(v) {}
+	explicit Float4(float s) noexcept { value = set4(s); }
+	Float4(float x, float y, float z, float w) noexcept { value = set4(x, y, z, w); }
+	Float4(float4 v) noexcept : value(v) {}
 
-	operator float4::Type() const noexcept { return value; }
+	operator float4() const noexcept { return value; }
 	//float operator[](int i) const noexcept { /*TODO*/ }
 	Float4 operator+() const noexcept { return *this; }
-	Float4 operator-() const noexcept { return Float4(float4::neg4(value)); }
-	Float4& operator+=(const Float4 v) noexcept { value = float4::add4(value, v); return *this; }
-	Float4& operator-=(const Float4 v) noexcept { value = float4::sub4(value, v); return *this; }
-	Float4& operator*=(const Float4 v) noexcept { value = float4::mul4(value, v); return *this; }
-	Float4& operator*=(const float f) noexcept { value = float4::mul4(value, float4::set4(f)); return *this; }
-	Float4& operator/=(const Float4 v) noexcept { value = float4::div4(value, v); return *this; }
-	Float4& operator/=(const float f) noexcept { value = float4::div4(value, float4::set4(f)); return *this; }
-	//friend Float4 operator+(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::add4(v1, v2)); }
-	//friend Float4 operator-(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::sub4(v1, v2)); }
-	//friend Float4 operator*(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::mul4(v1, v2)); }
-	//friend Float4 operator*(const float f, const Float4 v) noexcept { return Float4(float4::mul4(float4::set4(f), v)); }
-	//friend Float4 operator*(const Float4 v, const float f) noexcept { return Float4(float4::mul4(v, float4::set4(f))); }
-	//friend Float4 operator/(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::div4(v1, v2)); }
-	//friend Float4 operator/(const float f, const Float4 v) noexcept { return Float4(float4::div4(float4::set4(f), v)); }
-	//friend Float4 operator/(const Float4 v, const float f) noexcept { return Float4(float4::div4(v, float4::set4(f))); }
-	Bool4 operator<(const Float4& v) const noexcept { return Bool4(float4::less(value, v)); }
-	Bool4 operator<=(const Float4& v) const noexcept { return Bool4(float4::lessEqual(value, v)); }
-	Bool4 operator>(const Float4& v) const noexcept { return Bool4(float4::greater(value, v)); }
-	Bool4 operator>=(const Float4& v) const noexcept { return Bool4(float4::greaterEqual(value, v)); }
-	Bool4 operator==(const Float4& v) const noexcept { return Bool4(float4::equal(value, v)); }
-	Bool4 operator!=(const Float4& v) const noexcept { return Bool4(float4::not4(float4::equal(value, v))); }
+	Float4 operator-() const noexcept { return Float4(neg4(value)); }
+	Float4& operator+=(const Float4 v) noexcept { value = add4(value, v); return *this; }
+	Float4& operator-=(const Float4 v) noexcept { value = sub4(value, v); return *this; }
+	Float4& operator*=(const Float4 v) noexcept { value = mul4(value, v); return *this; }
+	Float4& operator*=(float s) noexcept { value = mul4(value, set4(s)); return *this; }
+	Float4& operator/=(const Float4 v) noexcept { value = div4(value, v); return *this; }
+	Float4& operator/=(float s) noexcept { value = div4(value, set4(s)); return *this; }
+	//friend Float4 operator+(const Float4 v1, const Float4 v2) noexcept { return Float4(add4(v1, v2)); }
+	//friend Float4 operator-(const Float4 v1, const Float4 v2) noexcept { return Float4(sub4(v1, v2)); }
+	//friend Float4 operator*(const Float4 v1, const Float4 v2) noexcept { return Float4(mul4(v1, v2)); }
+	//friend Float4 operator*(float s, const Float4 v) noexcept { return Float4(mul4(set4(s), v)); }
+	//friend Float4 operator*(const Float4 v, float s) noexcept { return Float4(mul4(v, set4(s))); }
+	//friend Float4 operator/(const Float4 v1, const Float4 v2) noexcept { return Float4(div4(v1, v2)); }
+	//friend Float4 operator/(float s, const Float4 v) noexcept { return Float4(div4(set4(s), v)); }
+	//friend Float4 operator/(const Float4 v, float s) noexcept { return Float4(div4(v, set4(s))); }
+	Bool4 operator<(const Float4& v) const noexcept { return Bool4(less(value, v)); }
+	Bool4 operator<=(const Float4& v) const noexcept { return Bool4(lessEqual(value, v)); }
+	Bool4 operator>(const Float4& v) const noexcept { return Bool4(greater(value, v)); }
+	Bool4 operator>=(const Float4& v) const noexcept { return Bool4(greaterEqual(value, v)); }
+	Bool4 operator==(const Float4& v) const noexcept { return Bool4(equal(value, v)); }
+	Bool4 operator!=(const Float4& v) const noexcept { return Bool4(not4(equal(value, v))); }
 
-	float x() const noexcept { return float4::extract<float4::X>(value); }
-	float y() const noexcept { return float4::extract<float4::Y>(value); }
-	float z() const noexcept { return float4::extract<float4::Z>(value); }
-	float w() const noexcept { return float4::extract<float4::W>(value); }
-	Float4 xxxx() const noexcept { return Float4(float4::broadcast<float4::X>(value)); }
-	Float4 yyyy() const noexcept { return Float4(float4::broadcast<float4::Y>(value)); }
-	Float4 zzzz() const noexcept { return Float4(float4::broadcast<float4::Z>(value)); }
-	Float4 wwww() const noexcept { return Float4(float4::broadcast<float4::W>(value)); }
-	Float4 xyyy() const noexcept { return Float4(float4::swizzle<float4::XYYY>(value)); }
-	Float4 xzzz() const noexcept { return Float4(float4::swizzle<float4::XZZZ>(value)); }
-	Float4 xwww() const noexcept { return Float4(float4::swizzle<float4::XWWW>(value)); }
-	Float4 xyzz() const noexcept { return Float4(float4::swizzle<float4::XYZZ>(value)); }
-	Float4 xyww() const noexcept { return Float4(float4::swizzle<float4::XYWW>(value)); }
-	Float4 xzww() const noexcept { return Float4(float4::swizzle<float4::XZWW>(value)); }
-	Float4 yxxx() const noexcept { return Float4(float4::swizzle<float4::YXXX>(value)); }
-	Float4 yzzz() const noexcept { return Float4(float4::swizzle<float4::YZZZ>(value)); }
-	Float4 yzxx() const noexcept { return Float4(float4::swizzle<float4::YZXX>(value)); }
-	Float4 yzww() const noexcept { return Float4(float4::swizzle<float4::YZWW>(value)); }
-	Float4 ywww() const noexcept { return Float4(float4::swizzle<float4::YWWW>(value)); }
-	Float4 yxzw() const noexcept { return Float4(float4::swizzle<float4::YXZW>(value)); }
-	Float4 yxwz() const noexcept { return Float4(float4::swizzle<float4::YXWZ>(value)); }
-	Float4 zxxx() const noexcept { return Float4(float4::swizzle<float4::ZXXX>(value)); }
-	Float4 zxyy() const noexcept { return Float4(float4::swizzle<float4::ZXYY>(value)); }
-	Float4 zyyy() const noexcept { return Float4(float4::swizzle<float4::ZYYY>(value)); }
-	Float4 zyww() const noexcept { return Float4(float4::swizzle<float4::ZYWW>(value)); }
-	Float4 zwww() const noexcept { return Float4(float4::swizzle<float4::ZWWW>(value)); }
-	Float4 zwxy() const noexcept { return Float4(float4::swizzle<float4::ZWXY>(value)); }
-	Float4 wxxx() const noexcept { return Float4(float4::swizzle<float4::WXXX>(value)); }
-	Float4 wyyy() const noexcept { return Float4(float4::swizzle<float4::WYYY>(value)); }
-	Float4 wzzz() const noexcept { return Float4(float4::swizzle<float4::WZZZ>(value)); }
-	Float4 wzyx() const noexcept { return Float4(float4::swizzle<float4::WZYX>(value)); }
+	float x() const noexcept { return extract<X>(value); }
+	float y() const noexcept { return extract<Y>(value); }
+	float z() const noexcept { return extract<Z>(value); }
+	float w() const noexcept { return extract<W>(value); }
+	Float4 xxxx() const noexcept { return Float4(broadcast<X>(value)); }
+	Float4 yyyy() const noexcept { return Float4(broadcast<Y>(value)); }
+ 	Float4 zzzz() const noexcept { return Float4(broadcast<Z>(value)); }
+	Float4 wwww() const noexcept { return Float4(broadcast<W>(value)); }
+	Float4 xyyy() const noexcept { return Float4(swizzle4<XYYY>(value)); }
+	Float4 xzzz() const noexcept { return Float4(swizzle4<XZZZ>(value)); }
+	Float4 xwww() const noexcept { return Float4(swizzle4<XWWW>(value)); }
+	Float4 xyzz() const noexcept { return Float4(swizzle4<XYZZ>(value)); }
+	Float4 xyww() const noexcept { return Float4(swizzle4<XYWW>(value)); }
+	Float4 xzww() const noexcept { return Float4(swizzle4<XZWW>(value)); }
+	Float4 yxxx() const noexcept { return Float4(swizzle4<YXXX>(value)); }
+	Float4 yzzz() const noexcept { return Float4(swizzle4<YZZZ>(value)); }
+	Float4 yzxx() const noexcept { return Float4(swizzle4<YZXX>(value)); }
+	Float4 yzww() const noexcept { return Float4(swizzle4<YZWW>(value)); }
+	Float4 ywww() const noexcept { return Float4(swizzle4<YWWW>(value)); }
+	Float4 yxzw() const noexcept { return Float4(swizzle4<YXZW>(value)); }
+	Float4 yxwz() const noexcept { return Float4(swizzle4<YXWZ>(value)); }
+	Float4 zxxx() const noexcept { return Float4(swizzle4<ZXXX>(value)); }
+	Float4 zxyy() const noexcept { return Float4(swizzle4<ZXYY>(value)); }
+	Float4 zyyy() const noexcept { return Float4(swizzle4<ZYYY>(value)); }
+	Float4 zyww() const noexcept { return Float4(swizzle4<ZYWW>(value)); }
+	Float4 zwww() const noexcept { return Float4(swizzle4<ZWWW>(value)); }
+	Float4 zwxy() const noexcept { return Float4(swizzle4<ZWXY>(value)); }
+	Float4 wxxx() const noexcept { return Float4(swizzle4<WXXX>(value)); }
+	Float4 wyyy() const noexcept { return Float4(swizzle4<WYYY>(value)); }
+	Float4 wzzz() const noexcept { return Float4(swizzle4<WZZZ>(value)); }
+	Float4 wzyx() const noexcept { return Float4(swizzle4<WZYX>(value)); }
 
-	float4::Type value;
+	float4 value;
 };
 
-inline Float4::Bool4 operator&&(const Float4::Bool4 b1, const Float4::Bool4 b2) noexcept { return Float4::Bool4(float4::and4(b1, b2)); }
-inline Float4::Bool4 operator||(const Float4::Bool4 b1, const Float4::Bool4 b2) noexcept { return Float4::Bool4(float4::or4(b1, b2)); }
+inline Float4::Bool4 operator&&(const Float4::Bool4 b1, const Float4::Bool4 b2) noexcept { return Float4::Bool4(and4(b1, b2)); }
+inline Float4::Bool4 operator||(const Float4::Bool4 b1, const Float4::Bool4 b2) noexcept { return Float4::Bool4(or4(b1, b2)); }
 
-inline bool any(const Float4::Bool4 b) noexcept { return float4::any(b.value); }
-inline bool all(const Float4::Bool4 b) noexcept { return float4::all(b.value); }
+inline bool any(const Float4::Bool4 b) noexcept { return any(b.value); }
+inline bool all(const Float4::Bool4 b) noexcept { return all(b.value); }
 
-inline Float4 operator+(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::add4(v1, v2)); }
-inline Float4 operator-(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::sub4(v1, v2)); }
-inline Float4 operator*(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::mul4(v1, v2)); }
-inline Float4 operator*(const float f, const Float4 v) noexcept { return Float4(float4::mul4(float4::set4(f), v)); }
-inline Float4 operator*(const Float4 v, const float f) noexcept { return Float4(float4::mul4(v, float4::set4(f))); }
-inline Float4 operator/(const Float4 v1, const Float4 v2) noexcept { return Float4(float4::div4(v1, v2)); }
-inline Float4 operator/(const float f, const Float4 v) noexcept { return Float4(float4::div4(float4::set4(f), v)); }
-inline Float4 operator/(const Float4 v, const float f) noexcept { return Float4(float4::div4(v, float4::set4(f))); }
+inline Float4 operator+(const Float4 v1, const Float4 v2) noexcept { return Float4(add4(v1, v2)); }
+inline Float4 operator-(const Float4 v1, const Float4 v2) noexcept { return Float4(sub4(v1, v2)); }
+inline Float4 operator*(const Float4 v1, const Float4 v2) noexcept { return Float4(mul4(v1, v2)); }
+inline Float4 operator*(float s, const Float4 v) noexcept { return Float4(mul4(set4(s), v)); }
+inline Float4 operator*(const Float4 v, float s) noexcept { return Float4(mul4(v, set4(s))); }
+inline Float4 operator/(const Float4 v1, const Float4 v2) noexcept { return Float4(div4(v1, v2)); }
+inline Float4 operator/(float s, const Float4 v) noexcept { return Float4(div4(set4(s), v)); }
+inline Float4 operator/(const Float4 v, float s) noexcept { return Float4(div4(v, set4(s))); }
 
 } // namespace core::simd
 
--- Core/Common/Simd/Intrinsics.hpp
+++ Core/Common/Simd/Intrinsics.hpp
@@ -16,17 +16,50 @@
 #include "SseIntrinsics.hpp"
 #include "AvxIntrinsics.hpp"
 
-#ifdef SIMD_SSE
-#define SIMD_HAS_FLOAT4 1
-#else
+#ifndef SIMD_HAS_FLOAT4
 #define SIMD_HAS_FLOAT4 0
 #endif
 
 namespace core::simd {
 
 #ifdef SIMD_SSE
-//using namespace sse;
-namespace float4 = sse::float4;
+using namespace sse;
+#endif
+
+#ifdef SIMD_AVX
+using namespace avx;
+#endif
+
+#if defined(SIMD_SSE) || defined(SIMD_AVX)
+
+template<typename T> inline T xxxx(T v) { return broadcast<X>(v); }
+template<typename T> inline T yyyy(T v) { return broadcast<Y>(v); }
+template<typename T> inline T zzzz(T v) { return broadcast<Z>(v); }
+template<typename T> inline T wwww(T v) { return broadcast<W>(v); }
+template<typename T> inline T xyyy(T v) { return swizzle4<XYYY>(v); }
+template<typename T> inline T xzzz(T v) { return swizzle4<XZZZ>(v); }
+template<typename T> inline T xwww(T v) { return swizzle4<XWWW>(v); }
+template<typename T> inline T xyzz(T v) { return swizzle4<XYZZ>(v); }
+template<typename T> inline T xyww(T v) { return swizzle4<XYWW>(v); }
+template<typename T> inline T xzww(T v) { return swizzle4<XZWW>(v); }
+template<typename T> inline T yxxx(T v) { return swizzle4<YXXX>(v); }
+template<typename T> inline T yzzz(T v) { return swizzle4<YZZZ>(v); }
+template<typename T> inline T yzxx(T v) { return swizzle4<YZXX>(v); }
+template<typename T> inline T yzww(T v) { return swizzle4<YZWW>(v); }
+template<typename T> inline T ywww(T v) { return swizzle4<YWWW>(v); }
+template<typename T> inline T yxzw(T v) { return swizzle4<YXZW>(v); }
+template<typename T> inline T yxwz(T v) { return swizzle4<YXWZ>(v); }
+template<typename T> inline T zxxx(T v) { return swizzle4<ZXXX>(v); }
+template<typename T> inline T zxyy(T v) { return swizzle4<ZXYY>(v); }
+template<typename T> inline T zyyy(T v) { return swizzle4<ZYYY>(v); }
+template<typename T> inline T zyww(T v) { return swizzle4<ZYWW>(v); }
+template<typename T> inline T zwww(T v) { return swizzle4<ZWWW>(v); }
+template<typename T> inline T zwxy(T v) { return swizzle4<ZWXY>(v); }
+template<typename T> inline T wxxx(T v) { return swizzle4<WXXX>(v); }
+template<typename T> inline T wyyy(T v) { return swizzle4<WYYY>(v); }
+template<typename T> inline T wzzz(T v) { return swizzle4<WZZZ>(v); }
+template<typename T> inline T wzyx(T v) { return swizzle4<WZYX>(v); }
+
 #endif
 
 } // namespace core::simd
