ef77122629fd6cbee6776b5484d76a917061122d
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-01-09 08:42:26 +0100

    Add Imaging header

---
+++ Core/Imaging/Imaging.hpp
@@ -0,0 +1,13 @@
+/*
+ *	Name: Imaging
+ *	Author: Pawel Mrochen
+ */
+
+#pragma once
+#ifndef CORE_IMAGING_HPP
+#define CORE_IMAGING_HPP
+
+#include "Color3.hpp"
+#include "Color4.hpp"
+
+#endif /* CORE_IMAGING_HPP */
