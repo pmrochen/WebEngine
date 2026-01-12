1c6ec9b0935458015f192f0d67acabf34d2d59a0
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-01-12 16:53:35 +0100

    Add Axes flags

---
+++ Foundation/Mathematics/Axes.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *  Name: Axes
+ *  Author: Pawel Mrochen
+ */
+
+using System;
+using System.ComponentModel;
+
+namespace Foundation.Mathematics
+{
+	[Flags]
+	public enum Axes // 1 << Axis
+	{
+		None = 0,
+		X = 1,
+		Y = 2,
+		[Browsable(false)] XY = 3,
+		Z = 4,
+		[Browsable(false)] XZ = 5,
+		[Browsable(false)] YZ = 6,
+		[Browsable(false)] XYZ = 7,
+		W = 8,
+		[Browsable(false)] XW = 9,
+		[Browsable(false)] YW = 10,
+		[Browsable(false)] XYW = 11,
+		[Browsable(false)] ZW = 12,
+		[Browsable(false)] XZW = 13,
+		[Browsable(false)] YZW = 14,
+		[Browsable(false)] XYZW = 15
+	}
+}
--- Foundation/Mathematics/Axis.cs
+++ Foundation/Mathematics/Axis.cs
@@ -14,17 +14,4 @@ namespace Foundation.Mathematics
 		Z,
 		W
 	}
-
-	//[Flags]
-	//public enum Axes // 1 << Axis
-	//{
-	//    None = 0,
-	//    X = 1,
-	//    Y = 2,
-	//    [Browsable(false)] XY = 3,
-	//    Z = 4,
-	//    [Browsable(false)] XZ = 5,
-	//    [Browsable(false)] YZ = 6,
-	//    [Browsable(false)] XYZ = 7
-	//}
 }
