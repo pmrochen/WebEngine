1ca6513f7496e386a8c7b020a032de88df71f299
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-02-09 19:46:44 +0100

    Remove the script from old location

--- Core/Replace.ps1
+++
@@ -1,6 +0,0 @@
-$content = [System.IO.File]::ReadAllText($args[0])
-for ($i=2; $i -le $args.Count; $i += 2)
-{
-	$content = $content.Replace($args[$i], $args[$i + 1])
-}
-[System.IO.File]::WriteAllText($args[1], $content)
