8f5c2eddd6b29426428ebc5a1a407751186ac760
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-04-02 13:58:23 +0200

    Move function definitions to header file

--- Core/Graphics/SamplerState.hpp
+++ Core/Graphics/SamplerState.hpp
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <limits>
 #include <algorithm>
 #include <functional>
 #include <iterator>
@@ -30,10 +31,49 @@ inline std::size_t hashRange(I first, I last) noexcept
 
 struct SamplerState
 {
-	SamplerState() noexcept;
-	explicit SamplerState(TextureWrapMode wrapMode) noexcept;
-	SamplerState(TextureWrapMode wrapMode, TextureFilter filter) noexcept;
-	SamplerState(TextureWrapMode wrapMode, TextureFilter filter, int maxAnisotropy) noexcept;
+	SamplerState() noexcept :
+		wrapModes((int)TextureWrapMode::REPEAT | ((int)TextureWrapMode::REPEAT << 2) | ((int)TextureWrapMode::REPEAT << 4)),
+		filters((int)TextureFilter::LINEAR | ((int)TextureFilter::LINEAR << 2) | ((int)TextureFilter::LINEAR << 4)),
+		maxAnisotropy(16),
+		depthFunctionSrgbDecoding((int)ComparisonFunction::NEVER | 0x80),
+		minMipLevel(-std::numeric_limits<float>::infinity()),
+		maxMipLevel(std::numeric_limits<float>::infinity()),
+		mipLodBias()
+	{
+	}
+
+	explicit SamplerState(TextureWrapMode wrapMode) noexcept :
+		wrapModes(((int)wrapMode & 3) | (((int)wrapMode & 3) << 2) | (((int)wrapMode & 3) << 4)),
+		filters((int)TextureFilter::LINEAR | ((int)TextureFilter::LINEAR << 2) | ((int)TextureFilter::LINEAR << 4)),
+		maxAnisotropy(16),
+		depthFunctionSrgbDecoding((int)ComparisonFunction::NEVER | 0x80),
+		minMipLevel(-std::numeric_limits<float>::infinity()),
+		maxMipLevel(std::numeric_limits<float>::infinity()),
+		mipLodBias()
+	{
+	}
+
+	SamplerState(TextureWrapMode wrapMode, TextureFilter filter) noexcept :
+		wrapModes(((int)wrapMode & 3) | (((int)wrapMode & 3) << 2) | (((int)wrapMode & 3) << 4)),
+		filters(((int)filter & 3) | (((int)filter & 3) << 2) | (((int)filter & 3) << 4)),
+		maxAnisotropy(16),
+		depthFunctionSrgbDecoding((int)ComparisonFunction::NEVER | 0x80),
+		minMipLevel(-std::numeric_limits<float>::infinity()),
+		maxMipLevel(std::numeric_limits<float>::infinity()),
+		mipLodBias()
+	{
+	}
+
+	SamplerState(TextureWrapMode wrapMode, TextureFilter filter, int maxAnisotropy) noexcept :
+		wrapModes(((int)wrapMode & 3) | (((int)wrapMode & 3) << 2) | (((int)wrapMode & 3) << 4)),
+		filters(((int)filter & 3) | (((int)filter & 3) << 2) | (((int)filter & 3) << 4)),
+		maxAnisotropy((int8_t)maxAnisotropy),
+		depthFunctionSrgbDecoding((int)ComparisonFunction::NEVER | 0x80),
+		minMipLevel(-std::numeric_limits<float>::infinity()),
+		maxMipLevel(std::numeric_limits<float>::infinity()),
+		mipLodBias()
+	{
+	}
 
 	bool operator==(const SamplerState& state) const noexcept
 	{
@@ -52,9 +92,6 @@ struct SamplerState
 		ar(wrapModes, filters, maxAnisotropy, depthFunctionSrgbDecoding, minMipLevel, maxMipLevel, mipLodBias);
 	}
 
-	// Default
-	static const SamplerState& getDefault() noexcept;
-
 	// Wrap mode
 	TextureWrapMode getWrapModeU() const noexcept { return (TextureWrapMode)(wrapModes & 3); }
 	TextureWrapMode getWrapModeV() const noexcept { return (TextureWrapMode)((wrapModes >> 2) & 3); }
@@ -64,7 +101,10 @@ struct SamplerState
 	void setWrapModeW(TextureWrapMode wrapW) noexcept { wrapModes = (wrapModes & ~(3 << 4)) | (((int)wrapW & 3) << 4); }
 	void setWrapModes(TextureWrapMode wrapMode) noexcept { setWrapModes(wrapMode, wrapMode, wrapMode); }
 	void setWrapModes(TextureWrapMode wrapU, TextureWrapMode wrapVW) noexcept { setWrapModes(wrapU, wrapVW, wrapVW); }
-	void setWrapModes(TextureWrapMode wrapU, TextureWrapMode wrapV, TextureWrapMode wrapW) noexcept;
+	void setWrapModes(TextureWrapMode wrapU, TextureWrapMode wrapV, TextureWrapMode wrapW) noexcept
+	{
+		wrapModes = ((int)wrapU & 3) | (((int)wrapV & 3) << 2) | (((int)wrapW & 3) << 4);
+	}
 
 	// Texture filter
 	TextureFilter getMinificationFilter() const noexcept { return (TextureFilter)(filters & 3); }
@@ -74,11 +114,21 @@ struct SamplerState
 	void setMagnificationFilter(TextureFilter magFilter) noexcept { filters = (filters & ~(3 << 2)) | (((int)magFilter & 3) << 2); }
 	void setMipmapFilter(TextureFilter mipFilter) noexcept { filters = (filters & ~(3 << 4)) | (((int)mipFilter & 3) << 4); }
 	void setFilters(TextureFilter filter) noexcept { setFilters(filter, filter, filter); }
-	void setFilters(TextureFilter minFilter, TextureFilter magFilter, TextureFilter mipFilter) noexcept;
+	void setFilters(TextureFilter minFilter, TextureFilter magFilter, TextureFilter mipFilter) noexcept
+	{
+		filters = ((int)minFilter & 3) | (((int)magFilter & 3) << 2) | (((int)mipFilter & 3) << 4);
+	}
+
+	// Anisotropy	
 	int getMaxAnisotropy() const noexcept { return (int)maxAnisotropy; }
 	void setMaxAnisotropy(int maxAnisotropy) noexcept { this->maxAnisotropy = (std::int8_t)maxAnisotropy; }
+
+	// Depth comparison
 	ComparisonFunction getDepthComparisonFunction() const noexcept { return (ComparisonFunction)(depthFunctionSrgbDecoding & 0x7F); }
-	void setDepthComparisonFunction(ComparisonFunction function) noexcept;
+	void setDepthComparisonFunction(ComparisonFunction function) noexcept
+	{
+		depthFunctionSrgbDecoding = (depthFunctionSrgbDecoding & ~0x7F) | ((int)function & 0x7F);
+	}
 
 	// Mipmaps
 	float getMinMipmapLevel() const noexcept { return minMipLevel; }
@@ -90,10 +140,32 @@ struct SamplerState
 
 	// sRGB-linear conversion
 	bool isSrgbDecodingEnabled() const noexcept { return (depthFunctionSrgbDecoding & 0x80); }
-	void setSrgbDecodingEnabled(bool enabled) noexcept;
+	void setSrgbDecodingEnabled(bool enabled) noexcept
+	{
+		if (enabled)
+			depthFunctionSrgbDecoding |= 0x80;
+		else
+			depthFunctionSrgbDecoding &= ~0x80;
+	}
 
 	// Simplification
-	void simplify(int maxAnisotropy, bool mipmapped) noexcept;
+	void simplify(int maxAnisotropy, bool mipmapped) noexcept
+	{
+		if (!mipmapped)
+		{
+			filters &= ~(3 << 4); // set mipFilter to TextureFilter::NONE
+			//minMipLevel = 0.0f;
+			//maxMipLevel = 0.0f;
+			//mipLodBias = 0.0f;
+		}
+
+		if (((filters & 3) <= 1) && (((filters >> 2) & 3) <= 1) && (((filters >> 4) & 3) <= 1))
+			this->maxAnisotropy = 1;
+		else if (maxAnisotropy < (int)this->maxAnisotropy)
+			this->maxAnisotropy = (std::int8_t)maxAnisotropy;
+	}
+
+	static const SamplerState DEFAULT;
 
 	std::uint8_t wrapModes;					// 3*2 bits
 	std::uint8_t filters;					// 3*2 bits
@@ -103,6 +175,8 @@ struct SamplerState
 	float maxMipLevel;
 	float mipLodBias;						// 4*uint8_t + 3*float = 128 bits
 };
+
+//const SamplerState SamplerState::DEFAULT{};
 
 } // namespace graphics
 
