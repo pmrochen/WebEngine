12fa0d6875719c80adc410a024bed0ae44b35cad
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-04-16 12:59:30 +0200

    Add Sidedness enum; remove GeometryAttributes

---
+++ Core/Graphics/Sidedness.hpp
@@ -0,0 +1,16 @@
+/*
+ *	Name: Sidedness
+ *	Author: Pawel Mrochen
+ */
+
+#pragma once
+
+namespace graphics {
+
+enum class Sidedness
+{
+	SINGLE_SIDED,
+	DOUBLE_SIDED
+};
+
+} // namespace graphics
