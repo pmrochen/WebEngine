dc77e46cc816194ef276e166a3cf37559169ffca
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-05-07 15:52:08 +0200

    Move Filesystem.hpp to Source folder

--- Core/Filesystem/Filesystem.hpp
+++
@@ -1,9 +0,0 @@
-/*
- *	Name: Filesystem
- *	Author: Pawel Mrochen
- */
-
-#pragma once
-
-#include "Endianness.hpp"
-#include "Path.hpp"
---
+++ Core/Filesystem/Source/Filesystem/Filesystem.hpp
@@ -0,0 +1,15 @@
+/*
+ *	Name: Filesystem
+ *	Author: Pawel Mrochen
+ */
+
+#pragma once
+
+#include "Endianness.hpp"
+#include "FileAccessFlags.hpp"
+#include "FileOpenMode.hpp"
+#include "SeekOrigin.hpp"
+#include "FilesystemError.hpp"
+#include "FilesystemException.hpp"
+#include "Path.hpp"
+#include "VirtualFilesystem.hpp"
